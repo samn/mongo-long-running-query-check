18a889aa0182c91c1392fba00902d49780b0cf7f
Sam Neubardt <samneubardt@gmail.com>
2014-03-10 17:29:31 -0400

    Fix indentation problem in report_long_queries

--- mongo-long-running-query-check.py
+++ mongo-long-running-query-check.py
@@ -63,8 +63,8 @@ class MongoLongRunningQueryCheck():
 
     def report_long_queries(self):
         long_queries_output = self.get_long_running_queries()
-            event = construct_event(long_queries_output)
-            print json.dumps([event])
+        event = construct_event(long_queries_output)
+        print json.dumps([event])
 
 if __name__ == "__main__":
     parser = OptionParser()
